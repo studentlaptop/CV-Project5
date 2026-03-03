d5e3b852757b7acbe1fd97c5b105a4b1ceb9c1a6
Noah Bracelin <159675326+studentlaptop@users.noreply.github.com>
2026-03-03 06:59:55 -0500

    input button and image output

--- P5_starter_COCO_SSD.js
+++ P5_starter_COCO_SSD.js
@@ -1,15 +1,19 @@
 // 'cocoSsd' and 'tf' are available globally
 // because we loaded them via script tags in index.html
 
-const img = document.getElementById('img');
+let imgElement = document.getElementById('imageSrc');
+let inputElement = document.getElementById('fileInput');
 
+inputElement.addEventListener('change', (e) => {
+  imgElement.src = URL.createObjectURL(e.target.files[0]);
+});
 // Wait until the image is fully loaded
-img.onload = async () => {
+imgElement.onload = async () => {
   // Load the model
   const model = await cocoSsd.load();
 
   // Run detection
-  const predictions = await model.detect(img);
+  const predictions = await model.detect(imgElement);
 
   console.log('Predictions:', predictions);
 };
